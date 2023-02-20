3ec78097246b3c72f5753cbffc25ae0275b99aff
Robyn Hamelryck <r0797966@student.ucll.be>
2023-02-20 08:55:58 +0100

    first commit

---
+++ src/main/java/be/ucll/ip/minor/groep5610/Groep5610Application.java
@@ -0,0 +1,13 @@
+package be.ucll.ip.minor.groep5610;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groep5610Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Groep5610Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/ucll/ip/minor/groep5610/Groep5610ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.ip.minor.groep5610;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groep5610ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
