2663230dcc1f648423daa111d2eb3f968af38d59
Robyn <r0797966@ucll.be>
2023-02-21 09:48:24 +0100

    fix datum

--- src/main/java/be/ucll/ip/minor/groep5610/regatta/web/RegattaController.java
+++ src/main/java/be/ucll/ip/minor/groep5610/regatta/web/RegattaController.java
@@ -5,6 +5,7 @@ import be.ucll.ip.minor.groep5610.regatta.domain.RegattaService;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,14 +48,14 @@ public class RegattaController {
         RegattaDto regatta1 = new RegattaDto();
         regatta1.setWedstrijdNaam("wedstrijd1");
         regatta1.setClubNaam("club1");
-        regatta1.setDatum(LocalDate.parse("2023-02-20"));
+        regatta1.setDatum(LocalDate.now());
         regatta1.setMaxTeams(5);
         regatta1.setCategorie("categorie1");
 
         RegattaDto regatta2 = new RegattaDto();
         regatta2.setWedstrijdNaam("wedstrijd2");
         regatta2.setClubNaam("club2");
-        regatta2.setDatum(LocalDate.parse("2023-02-25"));
+        regatta2.setDatum(LocalDate.now().plusDays(5));
         regatta2.setMaxTeams(3);
         regatta2.setCategorie("categorie2");
 
