f5bb1428e20a704ab5c18f225dccea59018369fd
Robyn <r0797966@ucll.be>
2023-03-13 15:00:00 +0100

    h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO REGATTA(wedstrijd_naam, name, date, max_teams, categorie) VALUES ('wedstrijd1', 'd_club1', CURRENT_DATE(), 5, 'categorie1' );
+INSERT INTO REGATTA(wedstrijd_naam, name, date, max_teams, categorie) VALUES ('wedstrijd2', 'd_club2', CURRENT_DATE(), 3, 'categorie2' );
