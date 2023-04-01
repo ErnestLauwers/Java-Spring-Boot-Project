a506406e5f7783546ea5772dd29b683cf0a5dfe0
Sevara <sevara.manoza@student.ucll.be>
2023-04-01 16:32:07 +0200

    tests TeamDto implemented

---
+++ src/test/java/be/ucll/ip/minor/groep5610/team/TeamBuilder.java
@@ -0,0 +1,69 @@
+package be.ucll.ip.minor.groep5610.team;
+
+import be.ucll.ip.minor.groep5610.team.domain.Team;
+
+public class TeamBuilder {
+    private String name;
+    private String category;
+    private Integer passengers;
+    private String club;
+
+    private TeamBuilder() {
+
+    }
+
+    public static TeamBuilder aTeam() {
+        return new TeamBuilder();
+    }
+
+    public static TeamBuilder aValidTeamAlpha() {
+        return aTeam().withName("Alpha").withCategory("AbCd123").withPassengers(5).withClub("ClubA");
+    }
+
+    public static TeamBuilder aTeamWithNoName() {
+        return aTeam().withCategory("AbCd123").withPassengers(5).withClub("ClubA");
+    }
+
+    public static TeamBuilder aTeamWithNoCategory() {
+        return aTeam().withName("Alpha").withPassengers(5).withClub("ClubA");
+    }
+
+    public static TeamBuilder aTeamWithNoPassengers() {
+        return aTeam().withName("Alpha").withCategory("AbCd123").withClub("ClubA");
+    }
+
+    public static TeamBuilder aTeamWithNoClub() {
+        return aTeam().withName("Alpha").withCategory("AbCd123").withPassengers(5);
+    }
+
+    public TeamBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public TeamBuilder withCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public TeamBuilder withPassengers(int passengers) {
+        this.passengers = passengers;
+        return this;
+    }
+
+    public TeamBuilder withClub(String club) {
+        this.club = club;
+        return this;
+    }
+
+    public Team build() {
+        Team team = new Team();
+        team.setName(name);
+        team.setCategory(category);
+        team.setPassengers(passengers);
+        if (club != null) {
+            team.setClub(club);
+        }
+        return team;
+    }
+}
---
+++ src/test/java/be/ucll/ip/minor/groep5610/team/TeamDtoBuilder.java
@@ -0,0 +1,69 @@
+package be.ucll.ip.minor.groep5610.team;
+
+import be.ucll.ip.minor.groep5610.team.web.TeamDto;
+
+public class TeamDtoBuilder {
+    private String name;
+    private String category;
+    private Integer passengers;
+    private String club;
+
+    private TeamDtoBuilder() {
+
+    }
+
+    public static TeamDtoBuilder aTeam() {
+        return new TeamDtoBuilder();
+    }
+
+    public static TeamDtoBuilder aValidTeamAlpha() {
+        return aTeam().withName("Alpha").withCategory("AbCd123").withPassengers(5).withClub("ClubA");
+    }
+
+    public static TeamDtoBuilder aTeamWithNoName() {
+        return aTeam().withCategory("AbCd123").withPassengers(5).withClub("ClubA");
+    }
+
+    public static TeamDtoBuilder aTeamWithNoCategory() {
+        return aTeam().withName("Alpha").withPassengers(5).withClub("ClubA");
+    }
+
+    public static TeamDtoBuilder aTeamWithNoPassengers() {
+        return aTeam().withName("Alpha").withCategory("AbCd123").withClub("ClubA");
+    }
+
+    public static TeamDtoBuilder aTeamWithNoClub() {
+        return aTeam().withName("Alpha").withCategory("AbCd123").withPassengers(5);
+    }
+
+    public TeamDtoBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public TeamDtoBuilder withCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public TeamDtoBuilder withPassengers(int passengers) {
+        this.passengers = passengers;
+        return this;
+    }
+
+    public TeamDtoBuilder withClub(String club) {
+        this.club = club;
+        return this;
+    }
+
+    public TeamDto build() {
+        TeamDto team = new TeamDto();
+        team.setName(name);
+        team.setCategory(category);
+        team.setPassengers(passengers);
+        if (club != null) {
+            team.setClub(club);
+        }
+        return team;
+    }
+}
