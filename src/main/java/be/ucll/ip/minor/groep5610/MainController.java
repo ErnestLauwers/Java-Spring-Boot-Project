5fd0a91439a1c7281be85d0f15ced13f422e4a2a
Sevara <sevara.manoza@student.ucll.be>
2023-04-18 18:38:56 +0200

    login

---
+++ src/main/java/be/ucll/ip/minor/groep5610/MainController.java
@@ -0,0 +1,17 @@
+package be.ucll.ip.minor.groep5610;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
--- src/main/java/be/ucll/ip/minor/groep5610/config/SecurityConfig.java
+++ src/main/java/be/ucll/ip/minor/groep5610/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package be.ucll.ip.minor.groep5610.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
@@ -40,5 +42,16 @@ public class SecurityConfig {
         return (web) -> web.ignoring()
                 .requestMatchers("/css/**", "/api/**", "/error")
                 .requestMatchers(antMatcher("/h2/**")); // https://stackoverflow.com/questions/74680244/h2-database-console-not-opening-with-spring-security;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/home", true)
+                .failureUrl("/login?error=true");
+        return http.build();
     }
 }
